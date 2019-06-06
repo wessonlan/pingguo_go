ff4f0147f51aa5c069c95eaff0a07ad2fed6da0e
weikang.sang <weikang.sang@ly.com>
2019-06-06 12:16:26 +0800

    temp commit1

--- personal/views/project_views.py
+++ personal/views/project_views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from personal.models.project import Project
 
@@ -8,11 +9,22 @@ def project_manage(request):
     管理页面，登录成功默认页面
     """
     project_all = Project.objects.all()
-    return render(request, "project_manage.html", {"projects": project_all})
+    return render(request, "project_manage.html", {"projects": project_all,
+                                                   "type": "list"})
 
 @login_required
 def add_project(request):
     """
     添加项目
     """
-    return render(request, 'add_project.html')
+    if request.method == 'GET':
+        return render(request, 'project_manage.html', {"type": "add"})
+    elif request.method == 'POST':
+        name = request.POST.get('name', '')
+        describe = request.POST.get('describe', '')
+        status = request.POST.get('status', '')
+        if name == '':
+            return render(request, 'project_manage.html',{"type": "add",
+                                                          "name_error": "项目名称不能为空"})
+        Project.objects.create(name=name, describe=describe, status=status)
+        return HttpResponseRedirect("/project/")
--- test_platform/settings.py
+++ test_platform/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -44,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -74,10 +73,20 @@ WSGI_APPLICATION = 'test_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# MySql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': BASE_DIR+'/my.cnf',
+        },
     }
 }
 
